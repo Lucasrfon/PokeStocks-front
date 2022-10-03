ad94eee84fbb0f2ee1fee1c86d5b85e545c6e6f2
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-03 17:05:36 -0300

    feat: create exit button inside footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,39 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ImExit } from "react-icons/im";
 
 export default function Footer() {
-    return(
-        <Container>
-            <h6>Gotta Buy 'Em All</h6>
-            <p>ポケモンに害はありませんでした、<br /> このウェブサイトの作成中。</p>
-        </Container>
-    )
+  return (
+    <Container>
+      <StyledLink to="/">
+        <ImExit />
+      </StyledLink>
+      <h6>Gotta Buy 'Em All</h6>
+      <p>
+        ポケモンに害はありませんでした、
+        <br /> このウェブサイトの作成中。
+      </p>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    text-align: center;
-    color: #fecc09;
-    text-shadow: 0 0 3px #FF0000, 0px 0px 5px #0000FF;;
+  text-align: center;
+  color: #fecc09;
+  text-shadow: 0 0 3px #ff0000, 0px 0px 5px #0000ff;
 
-    h6 {
-        font-family: 'Press Start 2P', cursive;
-        font-size: 10px;
-        margin-bottom: 10px;
-    }
+  h6 {
+    font-family: "Press Start 2P", cursive;
+    font-size: 10px;
+    margin: 10px 0px;
+  }
 
-    p {
-        font-size: 8px;
-    }
-`
+  p {
+    font-size: 8px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: #373b13;
+  font-size: 34px;
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,118 +2,131 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { AiOutlineStock, AiFillQuestionCircle } from "react-icons/ai";
 import { MdWork, MdCatchingPokemon } from "react-icons/md";
-import { GiSwordsEmblem, GiTrophy, GiBookmarklet, GiRollingDices, GiFlyingFlag } from "react-icons/gi";
+import {
+  GiSwordsEmblem,
+  GiTrophy,
+  GiBookmarklet,
+  GiRollingDices,
+  GiFlyingFlag,
+} from "react-icons/gi";
 import { IoPersonCircle } from "react-icons/io5";
 import Logo from "../components/Logo";
 import Footer from "../components/Footer";
 
 export default function Home() {
-    return(
-        <Container>
-            <Logo />
-            <main>
-                <StyledLink to="/">
-                    <AiOutlineStock />
-                    <p>Market</p>
-                </StyledLink>
-                <StyledLink to="/">
-                    <MdWork />
-                    <p>Jobs</p>
-                </StyledLink>
-                <StyledLink to="/">
-                    <GiSwordsEmblem />
-                    <p>Battle</p>
-                </StyledLink>
-                <StyledIcognito to="/">
-                    <AiFillQuestionCircle />
-                    <p>???</p>
-                </StyledIcognito>
-                <StyledIcognito to="/">
-                    <AiFillQuestionCircle />
-                    <p>???</p>
-                </StyledIcognito>
-                {/* <StyledLink to="/">
+  return (
+    <Container>
+      <Logo />
+      <main>
+        <StyledLink to="/">
+          <AiOutlineStock />
+          <p>Market</p>
+        </StyledLink>
+        <StyledLink to="/">
+          <MdWork />
+          <p>Jobs</p>
+        </StyledLink>
+        <StyledLink to="/">
+          <GiSwordsEmblem />
+          <p>Battle</p>
+        </StyledLink>
+        <StyledIcognito to="/">
+          <AiFillQuestionCircle />
+          <p>???</p>
+        </StyledIcognito>
+        <StyledIcognito to="/">
+          <AiFillQuestionCircle />
+          <p>???</p>
+        </StyledIcognito>
+        {/* <StyledLink to="/">
                     <MdCatchingPokemon />
                     <p>Deck</p>
                 </StyledLink> */}
-                {/* <StyledLink to="/">
+        {/* <StyledLink to="/">
                     <GiTrophy />
                     <p>Achievements</p>
                 </StyledLink> */}
-                <StyledLink to="/">
-                    <GiBookmarklet />
-                    <p>Album</p>
-                </StyledLink>
-                <StyledIcognito to="/">
-                    <AiFillQuestionCircle />
-                    <p>???</p>
-                </StyledIcognito>
-                <StyledIcognito to="/">
-                    <AiFillQuestionCircle />
-                    <p>???</p>
-                </StyledIcognito>
-                {/* <StyledLink to="/">
+        <StyledLink to="/">
+          <GiBookmarklet />
+          <p>Album</p>
+        </StyledLink>
+        <StyledIcognito to="/">
+          <AiFillQuestionCircle />
+          <p>???</p>
+        </StyledIcognito>
+        <StyledIcognito to="/">
+          <AiFillQuestionCircle />
+          <p>???</p>
+        </StyledIcognito>
+        {/* <StyledLink to="/">
                     <GiRollingDices />
                     <p>Lottery</p>
                 </StyledLink> */}
-                {/* <StyledLink to="/">
+        {/* <StyledLink to="/">
                     <GiFlyingFlag />
                     <p>Missions</p>
                 </StyledLink> */}
-                <StyledLink to="/">
-                    <IoPersonCircle />
-                    <p>Profile</p>
-                </StyledLink>
-            </main>
-            <Footer />
-        </Container>
-    )
+        <StyledLink to="/">
+          <IoPersonCircle />
+          <p>Profile</p>
+        </StyledLink>
+      </main>
+      <Footer />
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    background-color: #dc0a2d;
-    height: 100vh;
+  background-color: #dc0a2d;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  main {
+    background-color: #f6f7f9;
+    width: 500px;
+    height: 400px;
     display: flex;
-    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-between;
     align-items: center;
-    justify-content: space-around;
+    border-radius: 60px;
+    box-shadow: 2px 2px 10px #2e3436;
+  }
+`;
 
-    main {
-        background-color: aliceblue;
-        width: 500px;
-        height: 400px;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        align-items: center;
-        border-radius: 60px;
-        box-shadow: 2px 2px 10px #2e3436;
-    }
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 15px;
+  text-decoration: none;
+  color: #dc0a2d;
+  text-shadow: 0 0 5px #f0bb42, 0 0 3px #0868a5;
+  width: 120px;
 
-    main > * {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin: 15px;
-    }
-
-    main > * :first-child {
-        font-size: 34px;
-        margin-bottom: 5px;
-    }
-`
-
-const StyledLink  = styled(Link)`
-    text-decoration: none;
-    color: #dc0a2d;
-    text-shadow: 0 0 5px #f0bb42, 0 0 3px #0868A5;
-    width: 120px;
+  > *:first-child {
+    font-size: 34px;
+    margin-bottom: 5px;
+  }
 `;
 
 const StyledIcognito = styled(Link)`
-    text-decoration: none;
-    color: #1A0E04;
-    text-shadow: 0 0 5px black;
-    width: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 15px;
+  text-decoration: none;
+  color: #1a0e04;
+  text-shadow: 0 0 5px black;
+  width: 120px;
+
+  > *:first-child {
+    font-size: 34px;
+    margin-bottom: 5px;
+  }
 `;
